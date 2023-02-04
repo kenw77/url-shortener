dc1651a713764b470d2cfbecaf9b73441d08bcd7
kenw77 <kenw77@gmx.com>
2023-02-04 14:53:15 +0100

    Changed mobile menu

--- script.js
+++ script.js
@@ -2,31 +2,33 @@
 
 let state = "closed";
 const menuIcon = document.querySelector(".menu-icon");
-const mobileMenu = document.getElementById("main-nav");
+const mobileMenu = document.getElementById("mobile-nav");
 
 const showMenu = () => {
 	if (state !== "open") {
 		let id = setInterval(frame, 1);
-		let pos = -100;
+		let height = 0;
 		function frame() {
 			mobileMenu.style.display = "block";
-			if (pos === 97) {
+			if (height === 400) {
 				clearInterval(id);
 			} else {
-				pos++;
-				mobileMenu.style.top = pos + "px";
+				height++;
+				mobileMenu.style.height = height + "px";
 			}
 		}
 		state = "open";
 	} else {
 		let id = setInterval(frame, 1);
 		let pos = mobileMenu.style.top;
+		pos = pos.replace(/[^0-9]/g, '');
 		function frame() {
-			mobileMenu.style.display = "block";
 			if (pos === -100) {
 				clearInterval(id);
 			} else {
+				console.log(pos);
 				pos--;
+				console.log(pos);
 				mobileMenu.style.top = pos + "px";
 			}
 		}
